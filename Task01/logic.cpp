bd46bef34e2155051809ca353665ece4bf11ff6d
darya-loboca <dasha.lobova.it@gmail.com>
2026-04-18 19:29:08 +0300

    Task01 solved

--- Task01/logic.cpp
+++ Task01/logic.cpp
@@ -1,1 +1,26 @@
 #include "logic.h"
+
+
+bool is_power_of_four(int number) {
+
+	//basic case
+	if (number == 1 || number == 4) {
+		return true;
+	}
+
+	//fool-proof
+	if (number <= 0 || number % 4 != 0) {
+		return false;
+	}
+
+	//recursion case
+	return is_power_of_four(number / 4);
+
+
+	//cycle case
+	/*while (number > 0) {
+		number /= 4;
+		return number % 4 == 0 ? true : false;
+	}*/
+
+}
